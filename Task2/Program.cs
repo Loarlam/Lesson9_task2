1055b62e35a51947774d3d228e0b5c30f6083c73
Loarlam <messiah02@mail.ru>
2020-03-27 12:59:37 +0300

    Создал несколько лямбда-выржаений; присвоил результат лямбда-выражений делегатов переменным; вывел на экран значения переменных.

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,6 +1,6 @@
 ﻿/*Используя Visual Studio, создайте проект по шаблону Console Application.  
 Создайте четыре лямбда оператора для выполнения арифметических действий: (Add – сложение, Sub – вычитание, Mul – умножение, Div – деление). 
-Каждый лямбда оператор должен принимать два аргумента и возвращать результат вычисления.Лямбда оператор деления должен делать проверку деления на ноль.
+Каждый лямбда оператор должен принимать два аргумента и возвращать результат вычисления. Лямбда оператор деления должен делать проверку деления на ноль.
 Написать программу, которая будет выполнять арифметические действия указанные пользователем.
 */
 using System;
@@ -9,15 +9,35 @@ namespace Task2
 {
     class Program
     {
-        delegate double CalculatorDelegate (int a, int b);
+        delegate double CalculatorDelegate(int a, int b);
 
         static void Main(string[] args)
         {
+            CalculatorDelegate Add, Sub, Mul, Del;
             int firstValue = 7, secondValue = 4;
-            CalculatorDelegate AddDelegate = (int firstDigit, int secondDigit) => { return firstDigit + secondDigit; };
-            CalculatorDelegate SubDelegate = (int firstDigit, int secondDigit) => { return firstDigit - secondDigit; };
-            CalculatorDelegate Mul = (int firstDigit, int secondDigit) => { return firstDigit * secondDigit; };
-            CalculatorDelegate Del = (int firstDigit, int secondDigit) => { return firstDigit / secondDigit; };
+            double resultAdd, resultSub, resultMul, resultDel;
+
+            Add = (int firstDigit, int secondDigit) => { return firstDigit + secondDigit; };
+            Sub = (int firstDigit, int secondDigit) => { return firstDigit - secondDigit; };
+            Mul = (int firstDigit, int secondDigit) => { return firstDigit * secondDigit; };
+            Del = (int firstDigit, int secondDigit) =>
+            {
+                if (secondDigit != 0) return firstDigit / secondDigit;
+                return 0;
+            };
+
+            resultAdd = Add(firstValue, secondValue);
+            resultSub = Sub(firstValue, secondValue);
+            resultMul = Mul(firstValue, secondValue);
+            resultDel = Del(firstValue, secondValue);
+
+            Console.WriteLine($"{firstValue} + {secondValue} = {resultAdd}");
+            Console.WriteLine($"{firstValue} - {secondValue} = {resultSub}");
+            Console.WriteLine($"{firstValue} * {secondValue} = {resultMul}");
+            if (Del(firstValue,secondValue)!=0) Console.WriteLine($"{firstValue} / {secondValue} = {resultDel}");
+            else Console.WriteLine("На ноль делить нельзя!");
+
+            Console.ReadKey();
         }
     }
 }
